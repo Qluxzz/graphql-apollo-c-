0ffc59053ab993dd4811ea2ec2d14e5661ba6f62
Qluxzz <andre.eric.andersson@gmail.com>
2023-12-08 16:08:34 +0100

    Generated types should be checked in

---
+++ frontend/src/gql/graphql.ts
@@ -0,0 +1,198 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  /** The `Date` scalar represents an ISO-8601 compliant date type. */
+  Date: { input: any; output: any; }
+};
+
+export type DateOperationFilterInput = {
+  eq?: InputMaybe<Scalars['Date']['input']>;
+  gt?: InputMaybe<Scalars['Date']['input']>;
+  gte?: InputMaybe<Scalars['Date']['input']>;
+  in?: InputMaybe<Array<InputMaybe<Scalars['Date']['input']>>>;
+  lt?: InputMaybe<Scalars['Date']['input']>;
+  lte?: InputMaybe<Scalars['Date']['input']>;
+  neq?: InputMaybe<Scalars['Date']['input']>;
+  ngt?: InputMaybe<Scalars['Date']['input']>;
+  ngte?: InputMaybe<Scalars['Date']['input']>;
+  nin?: InputMaybe<Array<InputMaybe<Scalars['Date']['input']>>>;
+  nlt?: InputMaybe<Scalars['Date']['input']>;
+  nlte?: InputMaybe<Scalars['Date']['input']>;
+};
+
+export type FloatOperationFilterInput = {
+  eq?: InputMaybe<Scalars['Float']['input']>;
+  gt?: InputMaybe<Scalars['Float']['input']>;
+  gte?: InputMaybe<Scalars['Float']['input']>;
+  in?: InputMaybe<Array<InputMaybe<Scalars['Float']['input']>>>;
+  lt?: InputMaybe<Scalars['Float']['input']>;
+  lte?: InputMaybe<Scalars['Float']['input']>;
+  neq?: InputMaybe<Scalars['Float']['input']>;
+  ngt?: InputMaybe<Scalars['Float']['input']>;
+  ngte?: InputMaybe<Scalars['Float']['input']>;
+  nin?: InputMaybe<Array<InputMaybe<Scalars['Float']['input']>>>;
+  nlt?: InputMaybe<Scalars['Float']['input']>;
+  nlte?: InputMaybe<Scalars['Float']['input']>;
+};
+
+export enum Genre {
+  Action = 'ACTION',
+  Comedy = 'COMEDY',
+  Drama = 'DRAMA',
+  Horror = 'HORROR',
+  Scifi = 'SCIFI'
+}
+
+export type GenreOperationFilterInput = {
+  eq?: InputMaybe<Genre>;
+  in?: InputMaybe<Array<Genre>>;
+  neq?: InputMaybe<Genre>;
+  nin?: InputMaybe<Array<Genre>>;
+};
+
+export type IntOperationFilterInput = {
+  eq?: InputMaybe<Scalars['Int']['input']>;
+  gt?: InputMaybe<Scalars['Int']['input']>;
+  gte?: InputMaybe<Scalars['Int']['input']>;
+  in?: InputMaybe<Array<InputMaybe<Scalars['Int']['input']>>>;
+  lt?: InputMaybe<Scalars['Int']['input']>;
+  lte?: InputMaybe<Scalars['Int']['input']>;
+  neq?: InputMaybe<Scalars['Int']['input']>;
+  ngt?: InputMaybe<Scalars['Int']['input']>;
+  ngte?: InputMaybe<Scalars['Int']['input']>;
+  nin?: InputMaybe<Array<InputMaybe<Scalars['Int']['input']>>>;
+  nlt?: InputMaybe<Scalars['Int']['input']>;
+  nlte?: InputMaybe<Scalars['Int']['input']>;
+};
+
+export type ListFilterInputTypeOfMovieFilterInput = {
+  all?: InputMaybe<MovieFilterInput>;
+  any?: InputMaybe<Scalars['Boolean']['input']>;
+  none?: InputMaybe<MovieFilterInput>;
+  some?: InputMaybe<MovieFilterInput>;
+};
+
+export type ListFilterInputTypeOfPersonFilterInput = {
+  all?: InputMaybe<PersonFilterInput>;
+  any?: InputMaybe<Scalars['Boolean']['input']>;
+  none?: InputMaybe<PersonFilterInput>;
+  some?: InputMaybe<PersonFilterInput>;
+};
+
+export type Movie = {
+  __typename?: 'Movie';
+  actors: Array<Person>;
+  genre: Genre;
+  id: Scalars['Int']['output'];
+  name: Scalars['String']['output'];
+  rating: Scalars['Float']['output'];
+  released: Scalars['Date']['output'];
+};
+
+export type MovieFilterInput = {
+  actors?: InputMaybe<ListFilterInputTypeOfPersonFilterInput>;
+  and?: InputMaybe<Array<MovieFilterInput>>;
+  genre?: InputMaybe<GenreOperationFilterInput>;
+  id?: InputMaybe<IntOperationFilterInput>;
+  name?: InputMaybe<StringOperationFilterInput>;
+  or?: InputMaybe<Array<MovieFilterInput>>;
+  rating?: InputMaybe<FloatOperationFilterInput>;
+  released?: InputMaybe<DateOperationFilterInput>;
+};
+
+export type Person = {
+  __typename?: 'Person';
+  firstName: Scalars['String']['output'];
+  id: Scalars['Int']['output'];
+  lastName: Scalars['String']['output'];
+  movies: Array<Movie>;
+};
+
+export type PersonFilterInput = {
+  and?: InputMaybe<Array<PersonFilterInput>>;
+  firstName?: InputMaybe<StringOperationFilterInput>;
+  id?: InputMaybe<IntOperationFilterInput>;
+  lastName?: InputMaybe<StringOperationFilterInput>;
+  movies?: InputMaybe<ListFilterInputTypeOfMovieFilterInput>;
+  or?: InputMaybe<Array<PersonFilterInput>>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  actor?: Maybe<Person>;
+  actors: Array<Person>;
+  movie?: Maybe<Movie>;
+  movies: Array<Movie>;
+};
+
+
+export type QueryActorArgs = {
+  where?: InputMaybe<PersonFilterInput>;
+};
+
+
+export type QueryActorsArgs = {
+  where?: InputMaybe<PersonFilterInput>;
+};
+
+
+export type QueryMovieArgs = {
+  where?: InputMaybe<MovieFilterInput>;
+};
+
+
+export type QueryMoviesArgs = {
+  where?: InputMaybe<MovieFilterInput>;
+};
+
+export type StringOperationFilterInput = {
+  and?: InputMaybe<Array<StringOperationFilterInput>>;
+  contains?: InputMaybe<Scalars['String']['input']>;
+  endsWith?: InputMaybe<Scalars['String']['input']>;
+  eq?: InputMaybe<Scalars['String']['input']>;
+  in?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+  ncontains?: InputMaybe<Scalars['String']['input']>;
+  nendsWith?: InputMaybe<Scalars['String']['input']>;
+  neq?: InputMaybe<Scalars['String']['input']>;
+  nin?: InputMaybe<Array<InputMaybe<Scalars['String']['input']>>>;
+  nstartsWith?: InputMaybe<Scalars['String']['input']>;
+  or?: InputMaybe<Array<StringOperationFilterInput>>;
+  startsWith?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type ActorQueryVariables = Exact<{
+  actorId: Scalars['Int']['input'];
+}>;
+
+
+export type ActorQuery = { __typename?: 'Query', actor?: { __typename?: 'Person', id: number, firstName: string, lastName: string, movies: Array<{ __typename?: 'Movie', id: number, name: string }> } | null };
+
+export type MovieQueryVariables = Exact<{
+  movieId: Scalars['Int']['input'];
+}>;
+
+
+export type MovieQuery = { __typename?: 'Query', movie?: { __typename?: 'Movie', id: number, name: string, genre: Genre, released: any, actors: Array<{ __typename?: 'Person', id: number, firstName: string, lastName: string }> } | null };
+
+export type MoviesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MoviesQuery = { __typename?: 'Query', movies: Array<{ __typename?: 'Movie', id: number, name: string, released: any, actors: Array<{ __typename?: 'Person', id: number }> }> };
+
+
+export const ActorDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Actor"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"actorId"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"actor"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"where"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"id"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"eq"},"value":{"kind":"Variable","name":{"kind":"Name","value":"actorId"}}}]}}]}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"firstName"}},{"kind":"Field","name":{"kind":"Name","value":"lastName"}},{"kind":"Field","name":{"kind":"Name","value":"movies"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]}}]} as unknown as DocumentNode<ActorQuery, ActorQueryVariables>;
+export const MovieDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Movie"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"movieId"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"movie"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"where"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"id"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"eq"},"value":{"kind":"Variable","name":{"kind":"Name","value":"movieId"}}}]}}]}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"genre"}},{"kind":"Field","name":{"kind":"Name","value":"released"}},{"kind":"Field","name":{"kind":"Name","value":"actors"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"firstName"}},{"kind":"Field","name":{"kind":"Name","value":"lastName"}}]}}]}}]}}]} as unknown as DocumentNode<MovieQuery, MovieQueryVariables>;
+export const MoviesDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Movies"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"movies"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"released"}},{"kind":"Field","name":{"kind":"Name","value":"actors"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}}]}}]}}]}}]} as unknown as DocumentNode<MoviesQuery, MoviesQueryVariables>;
